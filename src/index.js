ff3509ae3528702195197bd168d7e94014cfd685
baranero <jakub.baran2@gmail.com>
2024-01-28 11:25:02 +0100

    update stats and risk

--- src/index.js
+++ src/index.js
@@ -5,54 +5,56 @@ import ScrollReveal from "scrollreveal";
 import Swiper from "swiper";
 import { Autoplay, Navigation, Grid, Pagination } from "swiper/modules";
 import createThreadCard from "./components/ui/threats-card";
-import header from './components/header.html'
-import banner from './components/banner.html'
-import brandArea from './components/brand-area.html'
-import stats from './components/stats.html'
-import expertise from './components/expertise.html'
-import risk from './components/risk.html'
-import success from './components/success.html'
-import social from './components/social.html'
-import visual from './components/visual.html'
-import company from './components/company.html'
-import bakery from './components/bakery.html'
-import form from './components/form.html'
-import team from './components/team.html'
-import faq from './components/faq.html'
-import guide from './components/guide.html'
-import topFooter from './components/top-footer.html'
-import bottomFooter from './components/bottom-footer.html'
+import header from "./components/header.html";
+import banner from "./components/banner.html";
+import brandArea from "./components/brand-area.html";
+import stats from "./components/stats.html";
+import expertise from "./components/expertise.html";
+import risk from "./components/risk.html";
+import success from "./components/success.html";
+import social from "./components/social.html";
+import visual from "./components/visual.html";
+import company from "./components/company.html";
+import bakery from "./components/bakery.html";
+import form from "./components/form.html";
+import team from "./components/team.html";
+import faq from "./components/faq.html";
+import guide from "./components/guide.html";
+import topFooter from "./components/top-footer.html";
+import bottomFooter from "./components/bottom-footer.html";
 import { toggleMobileMenu } from "./js/mobile-menu";
 
 // Load partials .html in index.html
-document.getElementById('header-container').innerHTML = header;
-document.getElementById('banner-container').innerHTML = banner;
-document.getElementById('brand-area-container').innerHTML = brandArea;
-document.getElementById('stats-container').innerHTML = stats;
-document.getElementById('expertise-container').innerHTML = expertise;
-document.getElementById('risk-container').innerHTML = risk;
-document.getElementById('success-container').innerHTML = success;
-document.getElementById('social-container').innerHTML = social;
-document.getElementById('visual-container').innerHTML = visual;
-document.getElementById('company-container').innerHTML = company;
-document.getElementById('bakery-container').innerHTML = bakery;
-document.getElementById('form-container').innerHTML = form;
-document.getElementById('team-container').innerHTML = team;
-document.getElementById('faq-container').innerHTML = faq;
-document.getElementById('guide-container').innerHTML = guide;
-document.getElementById('top-footer-container').innerHTML = topFooter;
-document.getElementById('bottom-footer-container').innerHTML = bottomFooter;
+document.getElementById("header-container").innerHTML = header;
+document.getElementById("banner-container").innerHTML = banner;
+document.getElementById("brand-area-container").innerHTML = brandArea;
+document.getElementById("stats-container").innerHTML = stats;
+document.getElementById("expertise-container").innerHTML = expertise;
+document.getElementById("risk-container").innerHTML = risk;
+document.getElementById("success-container").innerHTML = success;
+document.getElementById("social-container").innerHTML = social;
+document.getElementById("visual-container").innerHTML = visual;
+document.getElementById("company-container").innerHTML = company;
+document.getElementById("bakery-container").innerHTML = bakery;
+document.getElementById("form-container").innerHTML = form;
+document.getElementById("team-container").innerHTML = team;
+document.getElementById("faq-container").innerHTML = faq;
+document.getElementById("guide-container").innerHTML = guide;
+document.getElementById("top-footer-container").innerHTML = topFooter;
+document.getElementById("bottom-footer-container").innerHTML = bottomFooter;
 
 // Mobile menu
 window.toggleMobileMenu = toggleMobileMenu;
 
-// Initializing components
+// Brand section button
 const viewPlansButton = Button("View Our Plans");
 document.getElementById("banner-logo-section").appendChild(viewPlansButton);
 
 // Function to animate numerical values
 function animateValue(obj, start, end, duration) {
   let startTimestamp = null;
+  obj.innerHTML = start;
+
   const step = (timestamp) => {
     if (!startTimestamp) startTimestamp = timestamp;
     const progress = Math.min((timestamp - startTimestamp) / duration, 1);
@@ -64,18 +66,21 @@ function animateValue(obj, start, end, duration) {
   window.requestAnimationFrame(step);
 }
 
-
-
 // Initializing ScrollReveal for animations
 const sr = ScrollReveal();
 sr.reveal(".stat-card", {
+  beforeReveal: function (el) {
+    const number = el.querySelector(".stat-number");
+    number.textContent = ""; // Ukryj wartość początkową przed rozpoczęciem animacji
+  },
   afterReveal: function (el) {
     const number = el.querySelector(".stat-number");
-    const endValue = parseInt(number.textContent.replace(/\D/g, ""));
-    animateValue(number, 0, endValue, 2000); // 2000 ms = 2 seconds
+    const endValue = parseInt(number.getAttribute("data-value")); // Przechowuj końcową wartość w atrybucie 'data-value'
+    animateValue(number, 0, endValue, 2000); // Rozpocznij animację od 0 do endValue w ciągu 2000 ms
   },
 });
 
+// Stats section button
 const contactUsButton = Button("Contact Us");
 document.getElementById("stats-section").appendChild(contactUsButton);
 
@@ -115,12 +120,10 @@ const swiperWithPagination = new Swiper(".swiper-container-with-pagination", {
   pagination: {
     el: ".swiper-pagination",
   },
-
 });
 
 const viewMoreButton = Button("View More");
 document.getElementById("view-more-button").appendChild(viewMoreButton);
-
 
 // Do poprawy
 const startReplyingButton = Button("Start Replying Faster");
@@ -161,19 +164,19 @@ document.getElementById("company-container").appendChild(discoverMoreButton);
 const consultationButton = Button("Consultation");
 document.getElementById("form").appendChild(consultationButton);
 
-document.querySelectorAll('.accordion-button').forEach(button => {
-  button.addEventListener('click', () => {
+document.querySelectorAll(".accordion-button").forEach((button) => {
+  button.addEventListener("click", () => {
     const accordionContent = button.nextElementSibling;
 
-    if (button.classList.contains('active')) {
-      button.classList.remove('active');
+    if (button.classList.contains("active")) {
+      button.classList.remove("active");
       accordionContent.style.maxHeight = 0;
     } else {
-      document.querySelectorAll('.accordion-button').forEach(otherButton => {
-        otherButton.classList.remove('active');
+      document.querySelectorAll(".accordion-button").forEach((otherButton) => {
+        otherButton.classList.remove("active");
         otherButton.nextElementSibling.style.maxHeight = 0;
       });
-      button.classList.add('active');
+      button.classList.add("active");
       accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
     }
   });
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,53 @@
 module.exports = {
   purge: {
-    content: ['./src/**/*.html', './src/**/*.js'],
-    enabled: process.env.NODE_ENV === 'production', // Włącz tylko w produkcji
+    content: ["./src/**/*.html", "./src/**/*.js"],
+    enabled: process.env.NODE_ENV === "production", // Włącz tylko w produkcji
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
     screens: {
-      sm: '391px',
-      md: '769px',
-      lg: '1441px',
-      xl: '1921px',
+      sm: "391px",
+      md: "769px",
+      lg: "1441px",
+      xl: "1921px",
     },
     extend: {
+      borderRadius: {
+        DEFAULT: "20px",
+        stats: "10px",
+      },
       spacing: {
-        '15px': '15px',
-        '44px': '44px',
+        "10px": "10px",
+        "20px": "20px",
+        "15px": "15px",
+        "44px": "44px",
       },
       animation: {
-        carousel: 'carousel 15s linear infinite'
+        carousel: "carousel 15s linear infinite",
+        rotate: "rotate 2s ease-in-out infinite"
       },
       keyframes: {
         carousel: {
-          '0%': {transform: 'translateX(0)'},
-          '100%': {transform: 'translateX(-272%)'},
-        }
+          "0%": { transform: "translateX(0)" },
+          "100%": { transform: "translateX(-272%)" },
+        },
+        rotate: {
+          '0%': { transform: "rotate(0deg)" },
+          '50%': { transform: "rotate(10deg)" },
+          '100%': { transform: "rotate(0deg)" },
+        },
       },
       fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
+        poppins: ["Poppins", "sans-serif"],
       },
       fontSize: {
-        sm: ['16px', '19.2px'],
-        base: ['24px', '33px'],
-        lg: ['32px', '38.4px'],
-        xl: ['35px', '42px'],
-        custom: ['35.2px', '38.7px'],
-        xxl: ['45.7px', '54.8px'],
-        xxxl: ['50px', '60px'],
+        sm: ["16px", "19.2px"],
+        base: ["24px", "33px"],
+        lg: ["32px", "38.4px"],
+        xl: ["35px", "42px"],
+        custom: ["35.2px", "38.7px"],
+        xxl: ["45.7px", "54.8px"],
+        xxxl: ["50px", "60px"],
       },
       colors: {
         dark: "#292F3E",
@@ -44,12 +56,12 @@ module.exports = {
         paragraphFirst: "#727272",
         paragraphSecond: "#666666",
         underline: "#191919",
-        menu: "#0b0b0b"
-      }
+        menu: "#0b0b0b",
+      },
     },
   },
   variants: {
-  extend: {},
+    extend: {},
   },
   plugins: [],
 };
